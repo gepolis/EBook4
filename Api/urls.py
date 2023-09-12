e4d473e9578ab3a1206847d283fa1bd073a3b6f5
gepolis <ivanaksenov2010@mail.ru>
2023-09-12 19:20:59 +0300

    update

--- Api/urls.py
+++ Api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path(r'events/', views.EventsViewSet.as_view()),
-    path(r'users/', views.UsersViewSet.as_view()),
-    path(r'profile/', views.ProfileViewSet.as_view()),
-    path(r'profile/<int:pk>/', views.ProfileViewSet.as_view()),
+    path(r'events/', EventsViewSet.as_view()),
+    path(r'users/', UsersViewSet.as_view()),
+    path(r'profile/', ProfileViewSet.as_view()),
+    path(r'profile/<int:pk>/', ProfileViewSet.as_view()),
 ]
