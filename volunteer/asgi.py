8549561888da1a0a57d502145c6ff8126fc2ab32
gepolis <ivanaksenov2010@mail.ru>
2023-09-13 19:41:02 +0300

    update

--- volunteer/asgi.py
+++ volunteer/asgi.py
@@ -11,8 +11,9 @@ import PersonalArea.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volunteer.settings')
 
+django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
-    "https": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             PersonalArea.routing.websocket_urlpatterns
