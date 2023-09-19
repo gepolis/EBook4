9304afae4975e8f619bf35feb59b1a9030477111
gepolis <ivanaksenov2010@mail.ru>
2023-09-19 16:54:50 +0300

    update

--- PersonalArea/views/developer.py
+++ PersonalArea/views/developer.py
@@ -7,19 +7,25 @@ from django.shortcuts import render, redirect
 from Accounts import decorators
 from MainApp.models import FeedBackQuestions
 
+
 @decorators.is_developer
 def feedbacks(request):
     fbs = FeedBackQuestions.objects.all().filter(answer__isnull=True)
     return render(request, "developer/feedbacks.html", {"fbs": fbs, "archive": False, "section": "feedbacks"})
 
+
 @decorators.is_developer
 def feedbacks_archive(request):
     fbs = FeedBackQuestions.objects.all().filter(answer__isnull=False)
     return render(request, "developer/feedbacks.html", {"archive": True, "fbs": fbs, "section": "archive"})
+
+
 @decorators.is_developer
 def view_feedback(request, fb_id):
     fb = FeedBackQuestions.objects.get(pk=fb_id)
     return render(request, "developer/view_feedback.html", {"fb": fb, "section": "feedbacks"})
+
+
 @decorators.is_developer
 def send_feedback_answer(request, fb_id):
     if request.method == "POST":
@@ -30,6 +36,7 @@ def send_feedback_answer(request, fb_id):
         fb.answer_by = request.user
         fb.save()
         return redirect("/lk/feedbacks/")
+
 
 @decorators.is_developer
 def get_logs(request):
@@ -42,17 +49,15 @@ def get_logs(request):
         "403": "red",
         "404": "red",
         "500": "darkred",
-        'HTTP': "black",
-        '304': "red", # 304 - Not Modified
-        'Not Found': "darkred",
+        'HTTP': "#ADD8E6",
+        '304': "red",  # 304 - Not Modified
+
     }
-    if True:
-        lines = []
-        logs = ""
-        with open("/root/Ebook1/nohup.out", "r") as f:
-            lines = f.readlines()
-        logs = "<br>".join(lines)
-        for i in el_col:
-            logs = logs.replace(i, f"<span style='color: {el_col[i]}'>{i}</span>")
-        return render(request, "developer/logs.html", {"section": "logs", "logs": logs})
-    return redirect("/lk/")
+    lines = []
+    logs = ""
+    with open("/root/Ebook1/nohup.out", "r") as f:
+        lines = f.readlines()
+    logs = "<br>".join(lines)
+    for i in el_col:
+        logs = logs.replace(i, f"<span style='color: {el_col[i]}'>{i}</span>")
+    return render(request, "developer/logs.html", {"section": "logs", "logs": logs})
